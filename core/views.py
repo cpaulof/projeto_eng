ef9d578c135102cbcbd82027360e2ab4c283f9a9
cpaulof <paulof@acad.ifma.edu.br>
2020-12-07 13:07:39 -0300

    telas editar e criar

--- core/views.py
+++ core/views.py
@@ -66,71 +66,81 @@ def insercao(request):
         return HttpResponseForbidden()
 
     context = {}
-    if request.user.is_authenticated:
-        context['base_template'] = 'base_autenticado.html'
-    else:
-        context['base_template'] = 'base.html'
+    context['base_template'] =  _get_base_template(request)
     
     if request.method == "POST":
         navio = request.POST['navio']
-        if not navio.replace(' ', '').isalnum():
-            messages.error(request, "Identificação do navio inválida!")
+        navio = _get_navio_or_create(request, navio)
+        if navio is None:
             return HttpResponseRedirect(reverse_lazy('insercao'))
-        nome_navio = navio.upper()
         try:
-            navio = Navio.objects.get(nome=nome_navio)
-        except Navio.DoesNotExist:    
-            navio = Navio.objects.create(nome=nome_navio)
-            navio.save()   
-        solicitacao = Solicitacao.objects.create(usuario=request.user, navio=navio)
+            berco = int(request.POST['berco'])
+            berco = get_object_or_404(Berco, pk=berco)
+        except ValueError:
+            messages.error(request, "Informação de berço incorreta!")
+            return HttpResponseRedirect(reverse_lazy('insercao'))
+        solicitacao = Solicitacao.objects.create(usuario=request.user, navio=navio, berco=berco)
         solicitacao.save()
         messages.success(request, "Solicitação criada!")
         messages.success(request, "Você será notificado quando ela for atendida.")
-    
 
-
+    context['bercos'] = list(Berco.objects.all())
     return render(request, "core/insercao.html", context)
 
 def editar(request, atrid):
+    context = {}
     if not request.user.is_authenticated:
         return HttpResponseForbidden()
+    context['base_template'] = 'base_autenticado.html'
     if request.user.user_type in (1, 3):
         obj = get_object_or_404(Solicitacao, pk=atrid)
     else:
         obj = get_object_or_404(Solicitacao, usuario=request.user, pk=atrid)
-    #form = AtracacaoForm(instance=obj)
-    #form = AtracacaoForm2()
+    
+    if request.method == "POST":
+        try:
+            berco = int(request.POST['berco'])
+            berco = obj.berco = get_object_or_404(Berco, pk=berco)
+        except ValueError:
+            messages.error(request, "Informação de berço incorreta!")
+            return HttpResponseRedirect(reverse_lazy('editar', kwargs={"atrid": atrid}))
+        navio = request.POST['navio']
+        navio = _get_navio_or_create(request, navio)
+        if navio is None:
+            return HttpResponseRedirect(reverse_lazy('editar', kwargs={"atrid": atrid}))
+        obj.berco = berco
+        obj.navio = navio
+        obj.save()
+        messages.success(request, "Solicitação editada")
+        return HttpResponseRedirect(reverse_lazy('solicitacoes'))
 
-    return render(request, "core/editar.html", {'obj': obj})
+    context['obj'] = obj
+    context['bercos'] = list(Berco.objects.all())
+    return render(request, "core/editar.html", context)
+
 
 def visualizar(request):    
     context = {}
-    if request.user.is_authenticated:
-        context['base_template'] = 'base_autenticado.html'
-    else:
-        context['base_template'] = 'base.html'
-        
+    context['base_template'] =  _get_base_template(request)
     bercos = Berco.objects.all()
     solitacoes = [Solicitacao.objects.filter(berco=i,  status=1).order_by('data') for i in list(bercos)]
     context['solicitacoes'] = solitacoes
-
     return render(request, "core/visualizar.html", context)
+
 
 
 def logout_view(request):
     if request.user.is_authenticated:
         logout(request)
         messages.success(request, 'Deslogado')
-    
     return HttpResponseRedirect(reverse_lazy('index'))
 
     
 def solicitacoes(request):
     context = {}
-    context['base_template'] = 'base_autenticado.html'
-    
     if not request.user.is_authenticated:
         return HttpResponseForbidden()
+    context['base_template'] = 'base_autenticado.html'
     if request.user.user_type in (1, 3):
         result = Solicitacao.objects.filter(status=0).order_by('data')
     else:
@@ -144,10 +154,7 @@ def solicitacao(request, pk=None):
         return HttpResponseForbidden()
     context = {}
     user_type = request.user.user_type
-    if request.user.is_authenticated:
-        context['base_template'] = 'base_autenticado.html'
-    else:
-        context['base_template'] = 'base.html'
+    context['base_template'] =  _get_base_template(request)
     if user_type in (1, 3):
         obj = get_object_or_404(Solicitacao, pk=pk)
     else:
@@ -160,8 +167,30 @@ def solicitacao(request, pk=None):
 
 
 def confirmar_solicitacao(request, pk):
-    return HttpResponse("CONFIRMAR")
+    if not request.user.is_authenticated and request.user.user_type not in (1, 3):
+        return HttpResponseForbidden()
+    obj = get_object_or_404(Solicitacao, pk=pk)
+    obj.status = 1
+    obj.save()
+    return HttpResponse("CONFIRMAR" + str(pk))
 
 def excluir_solicitacao(request, pk):
     return HttpResponse("EXCLUIR")
+
+def _get_base_template(request):
+    if request.user.is_authenticated:
+        return 'base_autenticado.html'
+    return 'base.html'
+
+def _get_navio_or_create(request, navio):
+    if not navio.replace(' ', '').isalnum():
+        messages.error(request, "Identificação do navio inválida!")
+        return None
+    nome_navio = navio.upper()
+    try:
+        navio = Navio.objects.get(nome=nome_navio)
+    except Navio.DoesNotExist:    
+        navio = Navio.objects.create(nome=nome_navio)
+        navio.save() 
+    return navio
 
